aa3850716e31a4e69b835a3346dbc57734ffce40
gooyeduck <65698588+gooyeduck@users.noreply.github.com>
2022-03-28 18:17:33 +0600

    first commit

---
+++ Practice/Add Two Numbers.php
@@ -0,0 +1,8 @@
+<?php
+    /* You don't need to add/edit anything to the below solution.
+        Click on the SUBMIT button to solve your first problem on CodeChef.*/
+    fscanf(STDIN, "%d", $t);
+    while($t--){
+        fscanf(STDIN, "%d %d", $a, $b);
+        print ($a + $b) . "\n";
+    }
